55ee663f240312e900d345a4933ebd4cb06fd8f3
Matthew Nosworthy <mnosworthy007@gmail.com>
2024-09-15 13:25:57 +0100

    removed env vars

--- src/models/database/connection.ts
+++ src/models/database/connection.ts
@@ -2,9 +2,9 @@ import pgPromise from "pg-promise";
 const pgp = pgPromise({});
 
 export const db = pgp({
-  user: "postgres",
-  password: "kim2myarms",
-  host: "localhost",
-  port: 5432,
-  database: "website",
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  database: process.env.POSTGRES_DB,
 });
